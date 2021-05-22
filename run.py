45032f4b649c64f6a7abb2af474dde9f243c3566
owczarek72144 <owczarek_72144@cloud.wsb.wroclaw.pl>
2021-05-22 11:41:13 +0200

    Fix some warnings

--- run.py
+++ run.py
@@ -7,31 +7,35 @@ from forms import ContatcForm
 from flask import send_from_directory
 from flask_mail import Mail, Message
 from email_validator import validate_email, EmailNotValidError
-from flask import abort,redirect,url_for,make_response
-app=Flask(__name__)
+
+app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
+
 @app.route('/aboutme')
 def aboutme():
     return render_template("aboutme.html")
+
 
 @app.route('/gallery')
 def gallery():
     return render_template("gallery.html")
 
-@app.route('/contact', methods=['GET','POST'])
+
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
     form = ContatcForm()
-    message=''
+    message = ''
 
     if request.method == 'POST':
         email = request.form.get('email')
         try:
             validate_email(email)
-        except EmailNotValidError as e:
+        except EmailNotValidError:
             message = ('email', 'Nieprawidłowy adres mailowy')
             return render_template('contact_form/contact.html', form=form, message=message)
 
@@ -40,47 +44,49 @@ def contact():
         message = request.form.get('message')
 
         recipients = ['vassilli.zaitsev@gmail.com']
-        mesageToMe = Message(subject=subject,
-                      sender=app.config.get("MAIL_USERNAME"),
-                      recipients=recipients, # replace with your email for testing
-                      body="Message from: " + name + "\n" + "email: " + email + "\n" + "Message:"+ message)
-        messageToSender = Message(subject="Dziękuje za wysłanie wiadomośći",
-                      sender=app.config.get("MAIL_USERNAME"),
-                      recipients=list(email.split(" ")), # replace with your email for testing
-                      body="Dziękuję za wiadomość, wkrótce odpiszę")
-        mail.send(mesageToMe)
-        mail.send(messageToSender)
+        mesage_to_admin = Message(subject=subject,
+                                  sender=app.config.get("MAIL_USERNAME"),
+                                  recipients=recipients,  # replace with your email for testing
+                                  body="Message from: " + name + "\n" + "email: " + email + "\n" + "Message:" + message)
+        message_to_sender = Message(subject="Dziękuje za wysłanie wiadomośći",
+                                    sender=app.config.get("MAIL_USERNAME"),
+                                    recipients=list(email.split(" ")),  # replace with your email for testing
+                                    body="Dziękuję za wiadomość, wkrótce odpiszę")
+        mail.send(mesage_to_admin)
+        mail.send(message_to_sender)
 
         return render_template('contact_form/contactsuccess.html')
     elif request.method == 'GET':
         return render_template('contact_form/contact.html', form=form, message=message)
+
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-#errors
+
+# errors
 @app.errorhandler(404)
-def not_found_error(e):
+def not_found_error():
     return render_template('error_pages/404.html'), 404
 
+
 @app.errorhandler(500)
-def not_found_error(e):
+def not_found_error():
     return render_template('error_pages/500.html'), 500
 
-#email configuration
+
+# email configuration
 app.config.update(
     MAIL_SERVER='smtp.gmail.com',
     MAIL_PORT=465,
     MAIL_USE_SSL=True,
-    MAIL_USERNAME = 'vassilli.zaitsev@gmail.com',
-    MAIL_PASSWORD = 'qplzxmdxmzakrwei'
-    )
+    MAIL_USERNAME='vassilli.zaitsev@gmail.com',
+    MAIL_PASSWORD='qplzxmdxmzakrwei'
+)
 mail = Mail(app)
-
 
 app.secret_key = '289304jkdhfasd08f87sdayfasd89ufjsadfus'
 if __name__ == '__main__':
     app.run(debug=False)
-
